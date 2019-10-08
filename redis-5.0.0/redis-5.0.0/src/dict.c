ca79302b612b8a8fbecd4e11edc0d7a4e327fcfa
chensong <15850774503@163.com>
2019-10-08 12:02:39 +0800

    [添加server的结构体头文件]

--- redis-5.0.0/redis-5.0.0/src/dict.c
+++ redis-5.0.0/redis-5.0.0/src/dict.c
@@ -32,7 +32,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
+#include "server.h"
 #include "fmacros.h"
 
 #include <stdio.h>
